0fa905885c4dfe8c533a1b59de5625fff58bdbf0
ryanabest <ryanabest@gmail.com>
2019-02-28 21:45:56 -0500

    update d3tsv

--- exercises/d3tsv/js/index.js
+++ exercises/d3tsv/js/index.js
@@ -3,12 +3,31 @@ init();
 function init() {
   let svg = d3.select("body")
               .append("svg")
-              .attr("id","d3mouse-svg");
+              .attr("id","d3tsv-svg");
 
   let g = svg.append("g")
-             .attr("id","d3mouse-g");
+             .attr("id","d3tsv-g");
 
-  size();
+   d3.tsv("data/groceries.tsv").then((data) => {
+       // loop
+       data.forEach((d, i) => {
+           d3.select('svg')
+               .append('text')
+               .text(d.amount + ' | ' + d.unit + ' | ' + d.item + ' | ' + d.source)
+               // .attr("x", window.innerWidth/2)
+               // .attr("y", (i+1) * 30)
+               .attr("class","text")
+               .style('text-anchor', 'middle')
+               .style('fill', () => {
+                   if (d.source == 'store')
+                       return 'rgb(255, 120, 0)';
+                   else
+                       return 'darkgray';
+               });
+       });
+
+       size();
+   });
 
   window.addEventListener("resize", function() { size(); });
 }
@@ -23,8 +42,15 @@ function size() {
 
   setSvg();
 
+  d3.select("#d3tsv-svg").selectAll('.text').each(function(d,i) {
+    d3.select(this).attr('y',(i+1) * 30)
+                   .attr('x',widthSvg/2)
+  })
+  //   .attr('x',function(d,i){console.log(d);})
+
+
   function setSvg() {
-    d3.select("#d3mouse-svg")
+    d3.select("#d3tsv-svg")
       .attr("height",heightSvg)
       .attr("width",widthSvg);
   }
